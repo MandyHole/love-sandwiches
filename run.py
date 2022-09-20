d4d594f8d29084ac7c40080e84fd244fea2bc439
MandyHole <mandyhole17@gmail.com>
2022-09-20 13:41:27 +0000

    Get stock row for calculate_surplus_data function

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -55,8 +57,24 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print('sales worksheet updated successfully. \n')
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and see what surplus there is
+    """
+    print('calculating surplus...\n')
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
-data = get_sales_data()
-print(data)
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Runs all programme functions
+    """
+    data = get_sales_data()
+    print(data)
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+print("welcome to love sandwiches")
+main()
