73e0aad004e810cc7bebd88eab7fec5cf60563b5
MandyHole <mandyhole17@gmail.com>
2022-09-21 13:07:43 +0000

    Update function to work for updating all spreadsheets

--- run.py
+++ run.py
@@ -48,14 +48,14 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
+def update_worksheet(data, worksheet):
     """
     Adds data row to bottom of Sales worksheet in love_sandwiches spreadsheet
     """
-    print('updating sales worksheet \n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('sales worksheet updated successfully. \n')
+    print(f'updating {worksheet} worksheet \n')
+    updated_worksheet = SHEET.worksheet(worksheet)
+    updated_worksheet.append_row(data)
+    print(f'{worksheet} worksheet updated successfully. \n')
 
 def calculate_surplus_data(sales_row):
     """
@@ -70,6 +70,14 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+# def update_surplus_worksheet(value):
+#     """
+#     Add a row with the surplus for the day into the surplus worksheet
+#     """
+#     print('updating surplus worksheet \n')
+#     surplus_worksheet = SHEET.worksheet('surplus')
+#     surplus_worksheet.append_row(value)
+#     print('surplus worksheet updated \n')
 
 
 def main():
@@ -79,8 +87,8 @@ def main():
     data = get_sales_data()
     print(data)
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 print("welcome to love sandwiches")
 main()
